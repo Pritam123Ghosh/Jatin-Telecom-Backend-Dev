fbbe348ad1808a935ec3f9d258a322e481d85866
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-07-25 13:10:48 +0530

    Connected to the local db & created routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import connectDB from './db/dbConnect.js';
+import authRouter from './routes/auth.route.js';
 
+connectDB();
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
+
+app.use("/api/v1/auth", authRouter);
