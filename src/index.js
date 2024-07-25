741a6b6476d803dc2f650cb16fac93ccb5d73ea4
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-07-25 13:11:36 +0530

    Add comments

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import express from 'express';
 import connectDB from './db/dbConnect.js';
 import authRouter from './routes/auth.route.js';
 
+
+//Connection to DB
 connectDB();
 const app = express();
 app.use(express.json());
+//Connection to server
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 })
-
+//Routes
 app.use("/api/v1/auth", authRouter);
